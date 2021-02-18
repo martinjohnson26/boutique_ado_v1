b5a160ba6e43a3fb7d6aef50262f0aeeea7fa3b9
martinjohnson26 <martin.johnson26@outlook.com>
2021-02-18 12:02:50 +0000

    added basic checkout functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,7 +20,7 @@ def bag_contents(request):
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
-        })
+            })
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
